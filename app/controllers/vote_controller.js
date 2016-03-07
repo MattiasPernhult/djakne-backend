3d8c4b453decadb90997bc5e339091930320c6f9
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2016-03-07 20:09:05 +0100

    Added code from events to votes

--- app/controllers/vote_controller.js
+++ app/controllers/vote_controller.js
@@ -1,40 +1,78 @@
 
-var mongoService = require('../services/mongo_event_service');
+var mongoService = require('../services/mongo_coffee_service');
 var controller = {};
 
 controller.post = function (req, res) {
 
-console.log('i votes/ post');
- var coffeeToAdd={
+  console.log('i votes/ post');
+  var coffeeToAdd = {
 
     title: req.body.title,
     description: req.body.description,
-    totalVote: req.body.totalVote,
+    totalVotes: req.body.totalVotes,
     date: req.body.date,
     one: req.body.one,
     two: req.body.two,
     tree: req.body.tree,
     four: req.body.four,
     five: req.body.five,
- };
+  };
 
- mongoService.insertCoffee(coffeeToAdd, function (err, result) {
+  mongoService.insertCoffee(coffeeToAdd, function (err, result) {
 
-   if(err){
-     console.log(err);
-     return.res.status(500).send(err);
-   }
-   console.log('coffee To Add : ' + result.id);
-   res.send(result);
+    if(err){
+      console.log(err);
+      return.res.status(500).send(err);
+    }
+    console.log('Coffee To Add : ' + result.id);
+    res.send(result);
 
- });
+  });
 
-var getAvarageVotes = function(query, res){
+  var getAvarageVotes = function(query, res){
 
- var vote= {};
+    var vote = {};
 
+  };
 
+  controller.get = function(req, res) {
+    var query = {};
+    var queryParamsExists = Object.keys(req.query).length !== 0;
 
-};
+    if (queryParamsExists) {
+      query = buildQuery(req);
+    }
 
+    mongoService.getEvents(query, function(err, resultFromDB) {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }
+      var response = {
+        result: resultFromDB,
+      };
+      res.send(response);
+    });
+  };
+
+  var buildQuery = function(req) {
+    var query = {
+      date: {},
+    };
+    if (req.query.dateFrom !== undefined) {
+      query.date.$gte = new Date(req.query.dateFrom);
+    }
+    if (req.query.dateTo !== undefined) {
+      if (dateToIsValid) {
+        query.date.$lt = new Date(req.query.dateTo);
+      }
+    }
+    return query;
+  };
+
+  var dateToIsValid = function(dateFrom, dateTo) {
+    return new Date(dateTo).getDate() > new Date(dateFrom).getDate();
+  };
+
+  module.exports = controller;
 }
