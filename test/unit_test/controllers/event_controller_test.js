eaafa76c1c2207e2de173babe14ffdcf50fe8425
Felix Alhbin <alhbinfelix@gmail.com>
2016-03-07 09:55:41 +0100

    Added a directory for tests

---
+++ test/unit_test/controllers/event_controller_test.js
@@ -0,0 +1,11 @@
+
+var chaiAsPromised = require('chai-as-promised');
+var expect = chai.expect;
+chai.should();
+chai.use(chaiAsPromised)
+
+var eventController = require('../../../app/controllers/event_controller');
+
+describe('Testing the event controller', function() {
+    // TODO: Add tests for the event controller
+});
