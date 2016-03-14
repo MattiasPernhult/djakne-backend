b5585b80106292d91112b734f0c405a03bc336c4
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2016-03-14 09:24:45 +0100

    Added new routes

--- app/routes/coffee.js
+++ app/routes/coffee.js
@@ -7,10 +7,17 @@ var coffeeController = require('../controllers/coffee_controller');
  * Endpoint for /
  * HTTP method: GET
  */
-router.get('/', function(req, res, next) {
-  coffeeController.get(req, res);
+router.get('/history', function(req, res, next) {
+  coffeeController.getHistory(req, res);
 });
 
+router.get('/current', function(req, res, next) {
+  coffeeController.getCurrent(req, res);
+});
+
+router.get('/:id', function(req, res) {
+  coffeeController.getID(req.params.id);
+});
 /*
  * Endpoint for /
  * HTTP method: POST
@@ -20,6 +27,12 @@ router.post('/', function(req, res, next) {
   // TODO: anropa helper funktion som kollar att alla parametrar finns
   console.log('I coffee routes /coffee post');
   coffeeController.post(req, res);
+});
+
+router.post('/vote/:id', function(req, res, next) {
+  // TODO: anropa helper funktion som kollar att alla parametrar finns
+  console.log('I coffee routes /coffee post');
+  coffeeController.postVote(req, res);
 });
 
 
