b375dbd17ed4b0c2c17414fd8275d136c2a670f5
Mattias Pernhult <pernhultmattias@gmail.com>
2016-04-05 11:50:06 +0200

    refactor: Added limit parameter to GET req /member/today

--- app/controllers/member_controller.js
+++ app/controllers/member_controller.js
@@ -4,10 +4,18 @@ var mysqlService = require('../services/mysql_service');
 var helper = require('../utils/helper');
 
 controller.getPeopleAtDjakneToday = function(req, res) {
-  mysqlService.getPeopleAtDjakneToday(function(err, members) {
+  var query = {limit: undefined};
+  if (req.query.limit) {
+    if (isNaN(req.query.limit)) {
+      return res.status(400).send({error: 'Limit parameter must be a number'});
+    }
+    query.limit = Number(req.query.limit);
+  }
+  mysqlService.getPeopleAtDjakneToday(query, function(err, members) {
     if (err) {
-      return res.status(500).send('Ohh... database is having some stomach problems, ' +
-        'maybe to much coffee for one day...');
+      var errorMessage = 'Ohh... database is having some stomach problems, ' +
+        'maybe to much coffee for one day...';
+      return res.status(500).send({error: errorMessage});
     }
     return res.send({members: helper.sanitizeMembers(members.reverse(), true)});
   });
