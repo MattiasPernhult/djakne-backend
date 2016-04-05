86f1f1099f7072b7af92fdba0ea63f8bd492c990
Mattias Pernhult <pernhultmattias@gmail.com>
2016-04-05 10:59:39 +0200

    fix(): Added error handling when no users is fetch

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -8,8 +8,8 @@ auth.requiresLogin = function(req, res, next) {
     return res.status(400).send({message: 'Missing token'});
   }
   mysqlService.getUserByLinkedInToken(token, function(err, userid) {
-    if (!userid) {
-      return res.status(400).send({message: 'Invalid token'});
+    if (!userid || userid.length === 0) {
+      return res.status(400).send({message: 'Invalid token or user not found'});
     }
     req.body.userID = String(userid[0].id);
     next();
