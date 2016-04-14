097a61226fe91e71c190e6f8b27736868d2bc5a3
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2016-04-14 11:32:36 +0200

    Added views and fixed rotation retrotv

--- app/controllers/retro_tv_controller.js
+++ app/controllers/retro_tv_controller.js
@@ -7,11 +7,23 @@ var controller = {};
 
 controller.getRetrotv = function(req, res) {
 
-  
-
-
-  console.log('New get retrotv');
-  res.sendFile('app/views/orders.html', { root: '.' });
+  var d = new Date();
+  var n = d.getSeconds();
+  var file = '';
+  if (n >= 0 && n < 25) {
+    file = 'orders.html';
+  } else if (n >= 25 && n < 35) {
+    file = 'coffee.html';
+  } else if (n >= 35 && n < 50){
+    file = 'coworking.html';
+  } else if (n >= 50 && n < 60) {
+    file = 'giphy.html';
+  } else {
+    return res.status(500).send({
+      message: 'Old hardware do break from time to time...',
+    });
+  }
+  res.sendFile('app/views/' + file, { root: '.' });
 };
 
 module.exports = controller;
