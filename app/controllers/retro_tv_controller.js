dcf7685026b46052b6a5d1de69af2e90cf832ee4
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2016-04-19 15:31:16 +0200

    Fixed merge problem

--- app/controllers/retro_tv_controller.js
+++ app/controllers/retro_tv_controller.js
@@ -1,22 +1,69 @@
-var express = require('express');
-var server = express();
-var http = require('http');
-var httpServer = http.Server(server);
 var handlebars = require('handlebars');
 var fs = require('fs');
-var mongoService = require('../services/mongo_coffee_service')
+var request = require('request');
+var test = require('../data/member_today');
 var giphy = require('giphy-api')();
 
 var controller = {};
 
 controller.getRetrotv = function(req, res) {
   var data = {};
+  var body = {};
   var d = new Date();
   var n = d.getSeconds();
   var file = '';
   if (n >= 0 && n < 15) {
     file = 'orders.html';
-    renderAndSend(file, data, res);
+    request('http://localhost:4000/member/today', function(error, response, respBody) {
+      body = JSON.parse(respBody);
+      body = test;
+      if (!error && response.statusCode === 200) {
+        if (body.members[0] !== undefined) {
+          data.name0 = body.members[0].firstName + ' ' + body.members[0].lastName;
+          data.img0 = body.members[0].image;
+          data.headline0 = body.members[0].headline;
+          data.order0 = true;
+        }
+        if (body.members[1] !== undefined) {
+          data.name1 = body.members[1].firstName + ' ' + body.members[1].lastName;
+          data.img1 = body.members[1].image;
+          data.headline1 = body.members[1].headline;
+          data.order1 = true;
+        }
+        if (body.members[2] !== undefined) {
+          data.name2 = body.members[2].firstName + ' ' + body.members[2].lastName;
+          data.img2 = body.members[2].image;
+          data.headline2 = body.members[2].headline;
+          data.order2 = true;
+        }
+        if (body.members[3] !== undefined) {
+          data.name3 = body.members[3].firstName + ' ' + body.members[3].lastName;
+          data.img3 = body.members[3].image;
+          data.headline3 = body.members[3].headline;
+          data.order3 = true;
+        }
+        if (body.members[4] !== undefined) {
+          data.name4 = body.members[4].firstName + ' ' + body.members[4].lastName;
+          data.img4 = body.members[4].image;
+          data.headline4 = body.members[4].headline;
+          data.order4 = true;
+        }
+        if (body.members[5] !== undefined) {
+          data.name5 = body.members[5].firstName + ' ' + body.members[5].lastName;
+          data.img5 = body.members[5].image;
+          data.headline5 = body.members[5].headline;
+          data.order5 = true;
+        }
+      } else if (response.statusCode === 400) {
+
+      } else {
+        return res.status(500).send({
+          message: 'Ohh... database is having some stomach problems, ' +
+            'maybe to much coffee for one day...',
+        });
+      }
+      renderAndSend(file, data, res);
+    });
   } else if (n >= 15 && n < 35) {
     file = 'coffee.html';
     request('http://localhost:4000/coffee/current', function(error, response, respBody) {
@@ -41,7 +88,7 @@ controller.getRetrotv = function(req, res) {
       renderAndSend(file, data, res);
     });
   } else if (n >= 35 && n < 50) {
-    file = 'giphy.html';
+    file = 'coworking.html';
     renderAndSend(file, data, res);
   } else if (n >= 50 && n < 60) {
     file = 'giphy.html';
@@ -53,7 +100,7 @@ controller.getRetrotv = function(req, res) {
       limit: 2,
     }, function(err, res) {
       console.log(res);
-        data.img = res.data.url;
+      data.img = res.data.url;
     });
     renderAndSend(file, data, res);
   } else {
@@ -61,7 +108,6 @@ controller.getRetrotv = function(req, res) {
       message: 'Old hardware do break from time to time...',
     });
   }
-
 };
 
 function renderAndSend(file, data, res) {
@@ -80,21 +126,6 @@ function renderAndSend(file, data, res) {
     var html = template(data);
     res.send(html);
   });
-}
-
-function modifyJSON(object) {
-  object.averageVotes = 0;
-  object.totalVotes = 0;
-  object.totalVotes = object.one + object.two +
-    object.three + object.four + object.five;
-  if (object.totalVotes > 0) {
-    object.averageVotes = ((object.one + (object.two * 2) +
-      (object.three * 3) + (object.four * 4) +
-      (object.five * 5)) / object.totalVotes).toFixed(1);
-  }
-  delete object._id;
-  delete object.__v;
-  return object;
 }
 
 module.exports = controller;
